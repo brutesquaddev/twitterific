b8c1d1c7775a18ab9b34b7e3e8f72ef59cf3e1dc
David Terkula <david.terkula@kroger.com>
2018-07-24 15:28:57 -0400

    working on pulling tweets from twitter

--- src/main/kotlin/com/brutesquaddev/github/twitterific/controller/TwitterificController.kt
+++ src/main/kotlin/com/brutesquaddev/github/twitterific/controller/TwitterificController.kt
@@ -2,13 +2,38 @@ package com.brutesquaddev.github.twitterific.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import twitter4j.Query
+import twitter4j.TwitterFactory
+import twitter4j.Twitter
+import twitter4j.conf.ConfigurationBuilder
+import java.util.stream.Collectors
+import twitter4j.QueryResult
+
+
+
 
 @RestController
 class TwitterificController {
 
     @GetMapping("/tweets")
     fun getTweets() : List<String> {
-        return emptyList()
+
+        val cb = ConfigurationBuilder()
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(System.getProperty("consumer_key"))
+                .setOAuthConsumerSecret(System.getProperty("consumer_secret"))
+                .setOAuthAccessToken(System.getProperty("access_token"))
+                .setOAuthAccessTokenSecret(System.getProperty("access_secret"))
+        val tf = TwitterFactory(cb.build())
+        val twitter = tf.getInstance()
+
+        val query = Query("kroger")
+        val result = twitter.search(query)
+
+        return result.tweets.stream()
+                .map { item -> item.text }
+                .collect<List<String>>(Collectors.toList<String>())
+
     }
 
 }
